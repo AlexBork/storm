9adb1f140db2445bdee5e70a5e6e7260f15973c9
Alex Bork <alexander.bork@cs.rwth-aachen.de>
2026-08-21 19:45:22 +0200

    Change valuetype transformer to only rational-to-double

---
+++ src/storm/transformer/SparseRationalModelToDoubleTransformer.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <memory>
+
+#include "storm/adapters/RationalNumberForward.h"
+#include "storm/models/sparse/Model.h"
+
+namespace storm::transformer {
+
+/**
+ * Returns a model equivalent to @p inputModel with all probabilities, rates, and rewards converted to double.
+ *
+ * @pre inputModel is not null.
+ */
+std::shared_ptr<storm::models::sparse::Model<double>> sparseRationalModelToDouble(
+    std::shared_ptr<models::sparse::Model<storm::RationalNumber>> const& inputModel);
+
+}  // namespace storm::transformer
